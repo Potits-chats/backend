9967bf2d244ff885f2abb254098ee98cc4be1c79
FazCodeFR <30906528+FazCodeFR@users.noreply.github.com>
2023-10-13 12:09:46 +0200

    Add rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,31 @@ import { AppService } from './app.service';
 import { LoggerMiddleware } from './utils/logger.middleware';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [HttpModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    HttpModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    // Rate Limit : 100 requêtes par minute
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [Logger, AppService],
+  providers: [
+    Logger,
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
