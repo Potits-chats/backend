f22326f1d99d53b5b92e8f731c26b8f5fcf49ea8
FazCodeFR <30906528+FazCodeFR@users.noreply.github.com>
2024-06-03 14:17:29 +0200

    Fix contact test

--- src/contact/contact.controller.spec.ts
+++ src/contact/contact.controller.spec.ts
@@ -1,20 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ContactController } from './contact.controller';
 import { ContactService } from './contact.service';
+import { MailerService } from '@nestjs-modules/mailer';
 
 describe('ContactController', () => {
   let controller: ContactController;
+  let service: ContactService;
+
+  const mockMailerService = {
+    sendMail: jest.fn().mockResolvedValue(true),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ContactController],
-      providers: [ContactService],
+      providers: [
+        ContactService,
+        {
+          provide: MailerService,
+          useValue: mockMailerService,
+        },
+      ],
     }).compile();
 
     controller = module.get<ContactController>(ContactController);
+    service = module.get<ContactService>(ContactService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should call sendEmail on ContactService', async () => {
+    const createContactDto = {
+      nom: 'Test Name',
+      email: 'test@example.com',
+      message: 'Test message',
+    };
+    const spy = jest.spyOn(service, 'sendEmail').mockResolvedValue(undefined);
+
+    await controller.create(createContactDto);
+
+    expect(spy).toHaveBeenCalledWith(createContactDto);
   });
 });
--- src/contact/contact.service.spec.ts
+++ src/contact/contact.service.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ContactService } from './contact.service';
+import { MailerService } from '@nestjs-modules/mailer';
 
 describe('ContactService', () => {
   let service: ContactService;
 
+  const mockMailerService = {
+    sendMail: jest.fn().mockResolvedValue(true),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ContactService],
+      providers: [
+        ContactService,
+        {
+          provide: MailerService,
+          useValue: mockMailerService,
+        },
+      ],
     }).compile();
 
     service = module.get<ContactService>(ContactService);
@@ -14,5 +25,22 @@ describe('ContactService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should send an email', async () => {
+    const createContactDto = {
+      nom: 'Test Name',
+      email: 'test@example.com',
+      message: 'Test message',
+    };
+
+    await service.sendEmail(createContactDto);
+
+    expect(mockMailerService.sendMail).toHaveBeenCalledWith({
+      to: 'contact@potits-chats.com',
+      subject: 'Nouveau message de contact potits-chats (development)', // You may want to set NODE_ENV in your test environment
+      text: `Message de ${createContactDto.nom} (${createContactDto.email}): ${createContactDto.message}`,
+      html: `<p>Message de <b>${createContactDto.nom}</b> (${createContactDto.email}): ${createContactDto.message}</p>`,
+    });
   });
 });
