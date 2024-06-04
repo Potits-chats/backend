37e574867bf35abd74252274c929c4bd15fda0ec
marion-sgr <marion.segria@gmail.com>
2024-06-04 14:07:07 +0200

    rename chat by asso

--- src/associations/associations.service.ts
+++ src/associations/associations.service.ts
@@ -1,20 +1,85 @@
 import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { Associations, PrismaClient } from '@prisma/client';
+import * as cheerio from 'cheerio';
 
 @Injectable()
 export class AssociationsService {
   prisma = new PrismaClient();
   private readonly logger = new Logger(AssociationsService.name);
 
-  async findAll() {
-    const associations = await this.prisma.associations.findMany();
-    return associations;
+  async findAll(options: { chatId?: number; take?: number; skip?: number }) {
+    const { chatId, take, skip } = options;
+
+    let query = {
+      include: {
+        photos: true,
+        chat: true,
+        favoris: {
+          where: { utilisateurId: 1 }, // TODO CHANGER PAR L'ID DE L'UTILISATEUR CONNECTE
+        },
+      },
+      where: {
+        isVisible: true,
+      },
+    };
+    if (chatId) {
+      query = {
+        ...query,
+        where: {
+          ...query.where,
+          chat: {
+            id: chatId,
+          },
+        },
+      } as typeof query & { where: { chat: { id: number } } };
+    }
+
+    if (take) {
+      query = {
+        ...query,
+        take,
+      } as typeof query & { take: number };
+    }
+
+    if (skip) {
+      query = {
+        ...query,
+        skip,
+      } as typeof query & { skip: number };
+    }
+
+    const associations = await this.prisma.associations.findMany({
+      ...query,
+      orderBy: {
+        id: 'desc', // Sort associations by id in descending order
+      },
+    });
+
+    const truncatedAsso = associations.map((association: Associations) => {
+      const maxDescriptionLength = 130;
+
+      if (
+        association.description &&
+        association.description.length > maxDescriptionLength
+      ) {
+        const $ = cheerio.load(association.description);
+        const textWithoutHtml = $.text().slice(0, maxDescriptionLength) + '...';
+        association.description = textWithoutHtml;
+      }
+      return association;
+    });
+    return truncatedAsso;
   }
 
   async findOne(id: number) {
     const association = await this.prisma.associations.findUnique({
       where: {
         id: id,
+        isVisible: true,
+      },
+      include: {
+        photos: true,
+        chat: true,
       },
     });
     if (!association) {
@@ -24,6 +89,11 @@ export class AssociationsService {
   }
 
   async update(id: number, updateAssoDto: Associations) {
+    delete updateAssoDto.id;
+    delete updateAssoDto.chatId;
+    delete updateAssoDto['chat'];
+    delete updateAssoDto['photos'];
+    delete updateAssoDto['userId'];
     const associations = await this.prisma.associations.update({
       where: {
         id: id,
