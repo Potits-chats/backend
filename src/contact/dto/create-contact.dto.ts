1ddea14dcde49124900bb3e3fbb3e678472814b6
FazCodeFR <30906528+FazCodeFR@users.noreply.github.com>
2024-06-04 14:44:15 +0200

    Update dto contact

--- src/contact/dto/create-contact.dto.ts
+++ src/contact/dto/create-contact.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateContactDto {
+  @ApiProperty()
   nom: string;
+
+  @ApiProperty()
   email: string;
+
+  @ApiProperty()
   message: string;
 }
