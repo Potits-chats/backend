2529f0b3a84818fb337ede20f2fc5424aaf3b2cf
FazCodeFR <30906528+FazCodeFR@users.noreply.github.com>
2024-06-04 09:19:08 +0200

    Fix contact

--- src/contact/contact.service.spec.ts
+++ src/contact/contact.service.spec.ts
@@ -38,7 +38,10 @@ describe('ContactService', () => {
 
     expect(mockMailerService.sendMail).toHaveBeenCalledWith({
       to: 'contact@potits-chats.com',
-      subject: 'Nouveau message de contact potits-chats (development)', // You may want to set NODE_ENV in your test environment
+      subject:
+        'Nouveau message de contact potits-chats (' +
+        process.env.NODE_ENV +
+        ')',
       text: `Message de ${createContactDto.nom} (${createContactDto.email}): ${createContactDto.message}`,
       html: `<p>Message de <b>${createContactDto.nom}</b> (${createContactDto.email}): ${createContactDto.message}</p>`,
     });
