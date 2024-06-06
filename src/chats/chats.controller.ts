59608aa495e2a184867e339d018ebba1ddaaf838
FazCodeFR <30906528+FazCodeFR@users.noreply.github.com>
2024-06-06 13:57:08 +0200

    Test auto

--- src/chats/chats.controller.ts
+++ src/chats/chats.controller.ts
@@ -31,7 +31,7 @@ export class ChatsController {
   //   return this.chatsService.create(createChatDto);
   // }
 
-  // Test 4
+  // Test 1
   @ApiOperation({ summary: 'Récupération de tous les chats' })
   @ApiQuery({
     name: 'associationId',
